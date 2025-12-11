5aed6936a6d71673916f11ae436dec65e6a05659
Aaron Rosado <arosado@atlas.maas>
2025-12-11 00:40:49 +0000

    Add classes to index for easy import in es6.

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+import { AnnotationToolkit } from './annotationtoolkit.mjs';
+import { AnnotationToolbar } from './annotationtoolbar.mjs';
+import { FeatureCollectionUI } from './featurecollectionui.mjs';
+import { FeatureUI } from './featureui.mjs';
+import { FileDialog } from './filedialog.mjs';
+import { LayerUI } from './layerui.mjs';
+import { PaperOverlay } from './paper-overlay.mjs';
+import { RotationControlOverlay } from './rotationcontrol.mjs';
+import { RotationControlTool } from './rotationcontrol.mjs';
+import { AnnotationUITool } from './papertools/annotationUITool.mjs';
+
+export {
+    AnnotationToolkit,
+    AnnotationToolbar,
+    FeatureCollectionUI,
+    FeatureUI,
+    FileDialog,
+    LayerUI,
+    PaperOverlay,
+    RotationControlOverlay,
+    RotationControlTool,
+    AnnotationUITool,
+}
